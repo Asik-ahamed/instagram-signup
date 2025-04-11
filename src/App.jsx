fc46eb660719b2d38cabc06f54454b891a89b41b
asikahamed8144 <asik21223@gmail.com>
2025-04-11 12:48:28 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Instagram from "./Instagram";
+import Instagram from "./instagram";
 import { UserProvider } from "./UserContext";
-import UserTable from "./UserTable";
+import UserTable from "./usertable";
 import Home from "./Home";
 import Login from "./Login";
 
